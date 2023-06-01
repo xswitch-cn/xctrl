087f3237ad11845623738441ca89dae0c846f5e9
Seven Du <dujinfang@x-y-t.cn>
2023-06-01 09:43:22 +0800

    update fsds

--- fsds/fsds.go
+++ fsds/fsds.go
@@ -21,22 +21,17 @@ func FSDS(params *CallParams) string {
 
 	// Append the call parameters
 	if len(params.Params) > 0 {
+		comma := ""
 		sb.WriteString("{")
 		for key, value := range params.Params {
+			sb.WriteString(comma)
 			sb.WriteString(key)
 			sb.WriteString("=")
 			sb.WriteString(value)
-			sb.WriteString(",")
-		}
-		// Remove the trailing comma
-		str := sb.String()
-		if len(str) > 0 {
-			sb.Reset()
-			sb.WriteString(str[:len(str)-1])
+			comma = ","
 		}
 		sb.WriteString("}")
 	}
-
 	// Append the endpoint, profile, destination number, IP, port, and transport
 	sb.WriteString(params.Endpoint)
 	sb.WriteString("/")
@@ -53,6 +48,58 @@ func FSDS(params *CallParams) string {
 	} else {
 		log.Warn("Transport in params is wrong or not found")
 	}
+
+	return sb.String()
+}
+
+type User struct {
+	Domain string
+	Number string
+}
+
+func (u *User) String() string {
+	s := "user/" + u.Number
+	if u.Domain != "" {
+		s += "@" + u.Domain
+	}
+	return s
+}
+
+type File struct {
+	Path   string
+	Name   string
+	Params map[string]string
+}
+
+type PNGFile struct {
+	*File
+	MS    string
+	DText string
+}
+
+func (f *File) String() string {
+	var sb strings.Builder
+
+	// Append the file parameters
+	if len(f.Params) > 0 {
+		sb.WriteString("{")
+		for key, value := range f.Params {
+			sb.WriteString(key)
+			sb.WriteString("=")
+			sb.WriteString(value)
+			sb.WriteString(",")
+		}
+		// Remove the trailing comma
+		str := sb.String()
+		if len(str) > 0 {
+			sb.Reset()
+			sb.WriteString(str[:len(str)-1])
+		}
+		sb.WriteString("}")
+	}
+
+	// Append the file name
+	sb.WriteString(f.Path)
 
 	return sb.String()
 }
--- fsds/fsds_test.go
+++ fsds/fsds_test.go
@@ -21,3 +21,30 @@ func TestFSDS(t *testing.T) {
 	callString := FSDS(params)
 	fmt.Println(callString)
 }
+
+func TestUser(t *testing.T) {
+	u := &User{
+		Number: "1000",
+	}
+	if u.String() != "user/1000" {
+		t.Errorf("user.String() = %v, want %v", u.String(), "user/1000")
+	}
+	u.Domain = "test.test"
+	if u.String() != "user/1000@"+u.Domain {
+		t.Errorf("user.String() = %v, want %v", u.String(), "user/1000@"+u.Domain)
+	}
+}
+
+func TestFile(t *testing.T) {
+	file_string := "{png_ms=20000,dtext=请输入会议号}/tmp/test.jpg"
+	file := &File{
+		Path: "/tmp/test.jpg",
+		Params: map[string]string{
+			"png_ms": "20000",
+			"dtext":  "请输入会议号",
+		},
+	}
+	if file.String() != file_string {
+		t.Errorf("file.String() = %v, want %v", file.String(), file_string)
+	}
+}
