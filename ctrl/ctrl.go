9546c0905f7c002a113b6a13febb6dbe46a80af4
Seven Du <dujinfang@x-y-t.cn>
2023-09-04 10:44:22 +0800

    expose low level nats conn

--- ctrl/ctrl.go
+++ ctrl/ctrl.go
@@ -15,6 +15,7 @@ import (
 	"git.xswitch.cn/xswitch/xctrl/ctrl/bus"
 	"git.xswitch.cn/xswitch/xctrl/ctrl/nats"
 	"github.com/google/uuid"
+	natsio "github.com/nats-io/nats.go"
 )
 
 // Ctrl 控制中心
@@ -433,4 +434,12 @@ func OnEvicted(f func(string, interface{})) {
 	if globalCtrl != nil {
 		globalCtrl.OnEvicted(f)
 	}
+}
+
+func GetNATSConn() *natsio.Conn {
+	if globalCtrl == nil {
+		return nil
+	}
+
+	return globalCtrl.conn.GetConn()
 }
